6df381b2ee4e4c55df7706b6ed6a8629a817cfca
Ev <effraga@amazon.com>
2024-03-05 12:14:02 -0500

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -3,4 +3,3 @@ import {Anix} from '../src/anix.mjs'
 const eth = new Anix('eth', process.env.ENDPOINT_HTTP)
 
 console.dir(await eth.getBlockByNumber('latest', false))
-console.dir(await web3.sha3('blablabla'))
